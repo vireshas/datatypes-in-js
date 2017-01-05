8681b0e6b211859ec5faac685d9aca798868fe2a
Viresh S <asviresh@gmail.com>
2017-01-06 00:06:27 +0530

    tests pass

--- tests/validate.test.js
+++ tests/validate.test.js
@@ -2,16 +2,32 @@ import Validate from "../src/validate";
 
 
 describe('Validate', () => {
-  test('20 is a number', () => {
-    expect(Validate('number', 20)).toBe(true);
+  describe('numbers', () => {
+    test('20 is a number', () => {
+      expect(Validate('number', 20)).toBe(true);
+    })
+
+    test('0.3 is a number', () => {
+      expect(Validate('number', 0.3)).toBe(true);
+    })
+
+    test('abcd is not a number', () => {
+      expect(Validate('number', 'abcd')).toBe(false);
+    })
   })
 
-  test('0.3 is a number', () => {
-    expect(Validate('number', 0.3)).toBe(true);
-  })
+  describe('string', () => {
+    test('abc def is a string', () => {
+      expect(Validate('string', "abc def")).toBe(true);
+    })
 
-  test('abcd is a number', () => {
-    expect(Validate('number', 'abcd')).toBe(false);
+    test('0.3 is not a string', () => {
+      expect(Validate('string', 0.3)).toBe(false);
+    })
+
+    test('abcd is a string', () => {
+      expect(Validate('string', 'abcd')).toBe(true);
+    })
   })
 
 })
